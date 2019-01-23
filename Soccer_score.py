35ca1a33e4cfab3c897974af4c133c9fd451b966
tspies <tspies@student.wethinkcode.co.za>
2019-01-23 10:26:51 +0200

    Every working Project complete

--- Soccer_score.py
+++ Soccer_score.py
@@ -1,59 +1,41 @@
-# While Read One line
-#     Parse String into Array [team, score, team, score]
-#     compare score - Who Wins?
-#                   - Who Draw?
-#                   - Who Loses?
-#     FUNC If Win (A score > B score visa)
-#         Check if winning team is already in array
-#             if not - add to array [score - 1] element
-#         Check losing team is already in array
-#             if not add to array [score - 1] elemnt
-#         win team + 3
-
-#     FUNC If Draw (A score = B score)
-#         Check if both teams exit in array
-#             else add
-#         A score + 1 --- B score + 1
-
-#     sort by score
-#     Check for score duplicates
-#         Sort alphabetically by team names if duplicate
-
-#     Check length of dictionary
-#     While i < len(dictionary)
-#     print (i. "team name" "team score")
-
 import re
 soccer_dict = {}
 with open("soccer.txt") as file:
 	for line in file:
 		line = line.split(', ')
+		# Exracting the scores from line
 		score1 = re.findall(r"\d+", line[0])
 		score2 = re.findall(r"\d+", line[1])
+		# Extracting team names from line
 		team1 = re.sub(r'\d+', '', line[0])
 		team2 = re.sub(r'\d+', '', line[1])
-
+		# Stripping whitespace and newlines
 		team1 = team1.rstrip()
 		team2 = team2.rstrip()
 
+		# Checking if team already exists in soccer_dict - Adding if not
 		if ((team1 in soccer_dict) == False or (team2 in soccer_dict) == False):
 			if ((team1 in soccer_dict) == False):
 				soccer_dict[team1] = 0
 			if ((team2 in soccer_dict) == False):
 				soccer_dict[team2] = 0
+		# Checking which team won and adding score to soccer_dict value
 		if ((score1 > score2) or (score2 > score1)):
 			if (score1 > score2):
 				soccer_dict[team1] += 3
 			else:
 				soccer_dict[team2] += 3
+		# Adding score to both teams in case of a draw
 		elif (score1 == score2):
 			soccer_dict[team1] +=1
 			soccer_dict[team2] +=1
-i = 1
-for key, value in sorted(soccer_dict.items(), reverse=True, key=lambda kv: kv[1]):
+i = 0
+previous_score = -1
+for key, value in sorted(soccer_dict.iteritems(), key=lambda (k,v): (-v,k)):
+	if (value != previous_score):
+		i = i + 1
+		previous_score = value
 	if (value == 1):
 		print ('%d. %s %s pt' % (i, key, value))
-		i = i + 1
 	else:
 		print ('%d. %s %s pts' % (i, key, value))
-		i = i + 1
